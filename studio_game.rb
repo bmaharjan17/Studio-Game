ea1c35c1a67deee7d3353a0d856767694c5701b1
Bmaharjan <bmaharjan@fiduciary-trust.com>
2022-04-19 22:02:23 -0400

    adding game class and object interaction

--- studio_game.rb
+++ studio_game.rb
@@ -33,22 +33,43 @@ class Player
     
 end
 
+class Game
+
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+        @players = []
+    end
+
+    def add_player(a_player)
+        @players << a_player
+    end
+
+    def play
+        puts "There are #{@players.size} players in #{@title}"
+
+        @players.each do |player|
+            puts player 
+        end
+
+        @players.each do |player|
+            player.blam
+            player.w00t
+            player.w00t
+            puts player
+        end
+
+    end
+
+end
+
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-players = [player1, player2, player3]
-
-puts "There are #{players.size} players in the game"
-
-players.each do |p|
-    p.blam
-    p.w00t
-    p.w00t
-    puts p
-end
-
-players.pop
-player4 = Player.new("Shemp", 90)
-players.push(player4)
-puts players
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
